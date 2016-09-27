6f559ec774947d76a72c059b8fd1d51741c307f9
Pisotskiy Evgeniy <pisotskiy.jeny@gmail.com>
2016-09-27 13:55:50 +0300

    push on GitHub

--- equals/requirements/Person.java
+++ equals/requirements/Person.java
@@ -9,8 +9,8 @@ class Person {
         this.married = married;
     }
 
-    public boolean equals(Object o) {
-        if (o == this) return true; // You don't need check o for null, because it does instanceof
+    public boolean equals(Object o) { // You don't need check o for null, because it does instanceof
+        if (o == this) return true;
         if (o instanceof Person) {
             Person person = ((Person) o);
             return age == person.age && married == person.married;
